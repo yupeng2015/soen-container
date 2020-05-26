246b6a027243f3804bcfd6c3be841d2d8162188b
soen <970852318@qq.com>
2020-05-26 18:25:21 +0800

    feat

--- src/Container.php
+++ src/Container.php
@@ -6,5 +6,14 @@ namespace Soen\Container;
 
 class Container
 {
+    public $services = [];
+    public $servicesPath = '';
+    
+    function register(){
+        
+    }
 
+    function set(){
+
+    }
 }
