86ea9f52ec8dbbd6f8e67ad6ee68720fbe11d464
soen <970852318@qq.com>
2020-05-28 22:20:41 +0800

    feat

--- src/Application.php
+++ src/Application.php
@@ -11,11 +11,9 @@ class Application
     {
         $this->context = new ApplicationContext(new Container($path));
         \App::$app = $this;
-        var_dump(\App::$app->context->getComponent('router'));
-//        $this->context->getComponent('event');
     }
 
     function run(){
-//        \App::getComponent('router')->test();
+        context()->getComponent('router')->test();
     }
 }
