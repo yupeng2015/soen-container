dccefa6409da44589632ae6e35c4c71f192cb9e0
soen <970852318@qq.com>
2020-06-05 17:30:31 +0800

    feat

--- src/ApplicationContext.php
+++ src/ApplicationContext.php
@@ -8,22 +8,51 @@ use Psr\Container\ContainerInterface;
 
 class ApplicationContext
 {
+    /**
+     * @var Container
+     */
     public $container;
     function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
+    public function getComponentServer($name){
+        return $this->container->getServer($name);
+    }
+
+    public function setComponentServer($name, array $args = []){
+        $component = $this->container->get($name);
+        $reflection = new \ReflectionClass($component['class']);
+        $componentObject = $reflection->newInstanceArgs($args);
+        $this->container->setServer($name, $componentObject);
+    }
+    
     /**
      * @param $name
      * @return object
      * @throws \ReflectionException
      */
-    function getComponent($name){
-        $componentConfig = $this->container->get($name);
-        $reflection = new \ReflectionClass($componentConfig['class']);
-        $componentObject = $reflection->newInstanceArgs($componentConfig['args']);
+    function getComponent($name, array $args = []){
+        $component = $this->container->get($name);
+        $reflection = new \ReflectionClass($component['class']);
+        if (!empty($args)){
+            $component['args'] = $args;
+        }
+        $componentObject = $reflection->newInstanceArgs($component['args']);
         return $componentObject;
     }
+
+    /**
+     * @param $name
+     * @param array $args
+     * @throws \ReflectionException
+     */
+//    public function setComponent($name, array $args = []){
+//        $component = $this->container->get($name);
+//        $reflection = new \ReflectionClass($component['class']);
+//        $componentObject = $reflection->newInstanceArgs($args);
+//        $this->container->setServer($name, $componentObject);
+//    }
     
 }
--- src/Container.php
+++ src/Container.php
@@ -9,24 +9,42 @@ use Psr\Container\ContainerInterface;
 class Container implements ContainerInterface
 {
     public $services = [];
+    public $components = [];
     public $servicesPath = '';
 
     function __construct($path)
     {
         $server = new Server($path);
-        $this->services = $server->load();
+        $this->components = $server->load();
     }
 
     function get($id){
+        return $this->components[$id];
+    }
+
+    /**
+     * @param string $id
+     * @return mixed
+     */
+    function getServer(string $id){
         return $this->services[$id];
     }
 
     public function has($id)
     {
+        return isset($this->components[$id]);
+    }
+
+    public function hasServer($id)
+    {
         return isset($this->services[$id]);
     }
     
     public function add($id, $component){
-        $this->services[$id] = $component;
+        $this->components[$id] = $component;
+    }
+    
+    public function setServer($id, $componentObject){
+        $this->services[$id] = $componentObject;
     }
 }
